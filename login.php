f670bd274ae91065529e60648857f25e47f573af
Bushra <bushranawar17@gmail.com>
2021-03-14 01:06:39 +0600

    did ui part for login and register

---
+++ login.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/custom_style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	
+</head>
+<body>
+
+   <form>
+    <div class="container">
+    	
+
+        
+	   <div class="row">
+            <div class="col-md-6">
+
+	          <div class="login-header">
+                   <h1>Login Form</h1>
+
+	            </div>	
+		
+               
+		
+	         </div>	
+		
+	    </div>
+             
+             <div class="row">
+
+                
+                   <div class="col-md-6">
+                  
+                   		
+                   
+                   	  <div class="form-group" style="">
+                   
+                          <label for="email">Email</label>
+                           <input type="email" name="email" class="form-control"> 
+                   	
+                     
+                	
+                         </div>
+
+                    	
+                 </div>	
+                 	         	
+	            </div>
+	                <div class="row">
+	                	<div class="col-md-6">
+
+	                		<label for="password">Password</label>
+                           <input type="password" name="password" class="form-control"> 
+	                	</div>
+	                	
+	                </div>
+	                <div class="row">
+	               	  <div class="col-md-6 button-area-login">
+                         <button class="btn btn-default" type="submit" name="login_btn">Login</button>    
+	               	  	 <a href="#" class="btn btn-primary" role="button" name="register_btn">Register</a>
+	               	     </div>
+
+	            	
+	                 </div>
+	             
+	             
+
+    </div>
+     </form>
+</body>
+</html>
---
+++ registration.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/custom_style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	
+</head>
+<body>
+
+   <form>
+    <div class="container">
+    	
+
+        
+	   <div class="row">
+            <div class="col-md-6">
+
+	          <div class="login-header">
+                   <h1>Registration Form</h1>
+
+	            </div>	
+		
+               
+		
+	         </div>	
+		
+	    </div>
+             
+             <div class="row">
+
+                
+                   <div class="col-md-6">
+                  
+                   		
+                   
+                   	  <div class="form-group" style="">
+                   
+                          <label for="fname">First Name</label>
+                           <input type="text" name="fname" class="form-control"> 
+                   	
+                     
+                	
+                         </div>
+
+                    	
+                 </div>	
+                 	         	
+	            </div>
+	                <div class="row">
+	                	<div class="col-md-6">
+
+	                		<label for="lname">Last Name</label>
+                           <input type="text" name="lname" class="form-control"> 
+	                	</div>
+	                	
+	                </div>
+	                <div class="row">
+
+                
+                   <div class="col-md-6">
+                  
+                   		
+                   
+                   	  <div class="form-group" style="">
+                   
+                          <label for="email">Email</label>
+                           <input type="email" name="email" class="form-control"> 
+                   	
+                     
+                	
+                         </div>
+
+                    	
+                 </div>	
+                 	         	
+	            </div>
+	                <div class="row">
+	                	<div class="col-md-6">
+
+	                		<label for="pwd">Password</label>
+                           <input type="password" name="pwd" class="form-control"> 
+	                	</div>
+	                	
+	                </div>
+	                <div class="row">
+
+                
+                   <div class="col-md-6">
+                  
+                   		
+                   
+                   	  <div class="form-group" style="">
+                   
+                          <label for="cpwd">Confirm Password</label>
+                           <input type="password" name="cpwd" class="form-control"> 
+                   	
+                     
+                	
+                         </div>
+
+                    	
+                 </div>	
+                 	         	
+	            </div>
+	                <div class="row">
+	                	<div class="col-md-6">
+
+	                		<label for="contact_number">Contact Number</label>
+                           <input type="text" name="contact_number" class="form-control"> 
+	                	</div>
+	                	
+	                </div>
+	                <div class="row">
+
+                
+                   <div class="col-md-6">
+                  
+                   		
+                   
+                   	  <div class="form-group" style="">
+                   
+                          <label for="address">Address</label>
+                           <input type="text" name="address" class="form-control"> 
+                   	
+                     
+                	
+                         </div>
+
+                    	
+                 </div>	
+                 	         	
+	            </div>
+	               
+	                <div class="row">
+	               	  <div class="col-md-6 button-area-login">
+                         <button class="btn btn-default" type="submit" name="login_btn">Login</button>    
+	               	  	 <a href="#" class="btn btn-primary" role="button" name="register_btn">Register</a>
+	               	     </div>
+
+	            	
+	                 </div>
+	             
+	             
+
+    </div>
+     </form>
+</body>
+</html>
